f9816e6c22e39353c0f80c21a32fa073a75b4e8e
Alfred Ortega <omar.alfredo49@gmail.com>
2022-03-04 13:27:46 -0600

    Add skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { AuthorComponent } from './pages/admin/author/author.component';
+import { BookComponent } from './pages/admin/book/book.component';
+import { CategoryComponent } from './pages/admin/category/category.component';
+import { UserComponent } from './pages/admin/user/user.component';
+
+const routes: Routes = [
+  { path: "", redirectTo: "chat", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "chat", component: ChatComponent },
+  { path: "admin/autores", component: AuthorComponent },
+  { path: "admin/libros", component: BookComponent },
+  { path: "admin/categorias", component: CategoryComponent },
+  { path: "admin/usuarios", component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { CategoryComponent } from './pages/admin/category/category.component';
+import { AuthorComponent } from './pages/admin/author/author.component';
+import { BookComponent } from './pages/admin/book/book.component';
+import { UserComponent } from './pages/admin/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChatComponent,
+    CategoryComponent,
+    AuthorComponent,
+    BookComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
